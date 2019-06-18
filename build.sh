7b6929b8b51a4544825dbc98925b8fb112b58a4f
adamgreen <adam.green@kcl.ac.uk>
2019-06-18 22:02:26 +0100

    Added build script

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
-if $CONTEXT == "branch-deploy" then
+if [$CONTEXT == "branch-deploy"] 
+then
+    echo "deploy branch"
     bundle exec jekyll build --config _config_testing.yml
-elif $CONTEXT == "deploy-preview" then 
-    bundle exec jekyll build --config _config_testing.yml
-else
-    bundle exec jekyll build
+    exit
 fi
+if [$CONTEXT == "deploy-preview"] 
+then 
+    echo "deploy preview"
+    bundle exec jekyll build --config _config_testing.yml
+    exit
+fi
+
+bundle exec jekyll build
